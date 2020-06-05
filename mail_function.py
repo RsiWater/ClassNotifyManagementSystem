624d32f0ef72196a1be2ec940252626a4f3f7816
eric416 <eric23244@gmail.com>
2020-06-05 10:26:00 +0800

    origin

---
+++ mail_function.py
@@ -0,0 +1,49 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+
+def prepare(account,password):
+    smtp=smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(account,password)
+    return smtp
+
+def hw(schoolnum_l,score_l,account,smtp):
+    for i in range(len(schoolnum_l)):
+        from_addr=account
+        to_addr=schoolnum_l[i]+'@mail.nuk.edu.tw'
+
+        s='作業分數: '+score_l[i]+'分'
+        mime=MIMEText(s, "plain", "utf-8")
+        mime["Subject"]='作業'
+
+        msg=mime.as_string()
+        status=smtp.sendmail(from_addr, to_addr, msg)#加密文件，避免私密信息被截取
+        if status=={}:
+            print("郵件傳送成功!")
+        else:
+            print("郵件傳送失敗!")
+
+def roll_call(schoolnum_l,account,smtp):
+    for i in range(len(schoolnum_l)):
+        from_addr=account
+        to_addr=schoolnum_l[i]+'@mail.nuk.edu.tw'
+
+        s='點名成功'
+        mime=MIMEText(s, "plain", "utf-8")
+        mime["Subject"]='點名'
+
+        msg=mime.as_string()
+        status=smtp.sendmail(from_addr, to_addr, msg)#加密文件，避免私密信息被截取
+        if status=={}:
+            print("郵件傳送成功!")
+        else:
+            print("郵件傳送失敗!")
+
+def shutdown(smtp):
+    smtp.quit()
+    
+
+
+
